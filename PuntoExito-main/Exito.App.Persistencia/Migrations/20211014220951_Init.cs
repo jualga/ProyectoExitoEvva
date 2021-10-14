b41ba9adae0571d23c58c1fd7bbc4339175f1467
fabian <fabig4da@gmail.com>
2021-10-14 17:43:44 -0500

    Ventas section

---
+++ PuntoExito-main/Exito.App.Persistencia/Migrations/20211014220951_Init.cs
@@ -0,0 +1,294 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Exito.App.Persistencia.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Controles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cantidad = table.Column<int>(type: "int", nullable: false),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fabricante = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    precioCompra = table.Column<int>(type: "int", nullable: false),
+                    precioVenta = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Controles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    RolId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.RolId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sucursales",
+                columns: table => new
+                {
+                    SucursalId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sucursales", x => x.SucursalId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TypoDiscos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TypoDiscos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Empleados",
+                columns: table => new
+                {
+                    EmpleadoId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Cedula = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Apellido = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Usuario = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Clave = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    RolId = table.Column<int>(type: "int", nullable: false),
+                    SucursalId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Empleados", x => x.EmpleadoId);
+                    table.ForeignKey(
+                        name: "FK_Empleados_Roles_RolId",
+                        column: x => x.RolId,
+                        principalTable: "Roles",
+                        principalColumn: "RolId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Empleados_Sucursales_SucursalId",
+                        column: x => x.SucursalId,
+                        principalTable: "Sucursales",
+                        principalColumn: "SucursalId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Consolas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Almacenamiento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VelocidadRam = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    VelocidadProcesamiento = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TypoDiscoId = table.Column<int>(type: "int", nullable: true),
+                    ControlId = table.Column<int>(type: "int", nullable: false),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fabricante = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    precioCompra = table.Column<int>(type: "int", nullable: false),
+                    precioVenta = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Consolas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Consolas_Controles_ControlId",
+                        column: x => x.ControlId,
+                        principalTable: "Controles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Consolas_TypoDiscos_TypoDiscoId",
+                        column: x => x.TypoDiscoId,
+                        principalTable: "TypoDiscos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Compras",
+                columns: table => new
+                {
+                    CompraId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: false),
+                    Finalizada = table.Column<bool>(type: "bit", nullable: false),
+                    EmpleadoId = table.Column<int>(type: "int", nullable: false),
+                    ConsolaId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Compras", x => x.CompraId);
+                    table.ForeignKey(
+                        name: "FK_Compras_Consolas_ConsolaId",
+                        column: x => x.ConsolaId,
+                        principalTable: "Consolas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Compras_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "EmpleadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ventas",
+                columns: table => new
+                {
+                    VentaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Fecha = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Total = table.Column<int>(type: "int", nullable: false),
+                    Finalizada = table.Column<bool>(type: "bit", nullable: false),
+                    EmpleadoId = table.Column<int>(type: "int", nullable: false),
+                    ConsolaId = table.Column<int>(type: "int", nullable: false),
+                    Cantidad = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ventas", x => x.VentaId);
+                    table.ForeignKey(
+                        name: "FK_Ventas_Consolas_ConsolaId",
+                        column: x => x.ConsolaId,
+                        principalTable: "Consolas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Ventas_Empleados_EmpleadoId",
+                        column: x => x.EmpleadoId,
+                        principalTable: "Empleados",
+                        principalColumn: "EmpleadoId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoJuegos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ConsolaId = table.Column<int>(type: "int", nullable: true),
+                    Codigo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Version = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Fabricante = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    precioCompra = table.Column<int>(type: "int", nullable: false),
+                    precioVenta = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoJuegos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoJuegos_Consolas_ConsolaId",
+                        column: x => x.ConsolaId,
+                        principalTable: "Consolas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_ConsolaId",
+                table: "Compras",
+                column: "ConsolaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Compras_EmpleadoId",
+                table: "Compras",
+                column: "EmpleadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consolas_ControlId",
+                table: "Consolas",
+                column: "ControlId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Consolas_TypoDiscoId",
+                table: "Consolas",
+                column: "TypoDiscoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleados_RolId",
+                table: "Empleados",
+                column: "RolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Empleados_SucursalId",
+                table: "Empleados",
+                column: "SucursalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ventas_ConsolaId",
+                table: "Ventas",
+                column: "ConsolaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ventas_EmpleadoId",
+                table: "Ventas",
+                column: "EmpleadoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoJuegos_ConsolaId",
+                table: "VideoJuegos",
+                column: "ConsolaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Compras");
+
+            migrationBuilder.DropTable(
+                name: "Ventas");
+
+            migrationBuilder.DropTable(
+                name: "VideoJuegos");
+
+            migrationBuilder.DropTable(
+                name: "Empleados");
+
+            migrationBuilder.DropTable(
+                name: "Consolas");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Sucursales");
+
+            migrationBuilder.DropTable(
+                name: "Controles");
+
+            migrationBuilder.DropTable(
+                name: "TypoDiscos");
+        }
+    }
+}
